db2e823d4cf99087dcf3d9c2371fd70398d4861e
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2022-04-15 02:27:09 +0430

    adding css for login page and design ui login page:)

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,6 @@
 import logo from '../logo.svg';
 import './login.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Login() {
   return (
     <div className="wrapper">
@@ -8,7 +8,7 @@ function Login() {
         <h2 className="form-signin-heading">Please login</h2>
         <input type="text" className="form-control" name="username" placeHolder="Email Address" required="" autoFocus=""/>
         <input type="password" className="form-control" name="password" placeHolder="Password" required="" />
-        <label className="checkBox">
+        <label className="form-control" id="remember" >
           <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe" /> Remember Me
         </label>
         <button type="submit" className="btn btn-lg btn-primary btn-block">Login</button>
