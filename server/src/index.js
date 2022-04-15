691d5be8c84f50e042c992c9f972eaeabb038228
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2022-04-16 01:47:09 +0430

    send data login to /api/login (Mehdi)

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,15 @@
 import Express from 'express';
 import login from './routes/login.js'
-const app = Express();
+import cors from 'cors';
+// var cors = require('cors')
 
+
+const app = Express();
+app.use(cors()) // Use this after the variable declaration
 app.use(Express.json());
 app.use('/api/login', login);
 app.listen(3030, () => console.log('listening on port 3030'));
+
+
+
+// Use this after the variable declaration
