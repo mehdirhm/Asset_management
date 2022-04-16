9a04706fd65487c4a086434eec6eb751faca102f
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2022-04-16 15:22:36 +0430

    repair route server and send data for login to node server:) (Mehdi)

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -2,7 +2,13 @@ import { Router } from "express";
 
 const router = Router();
 
+
+
+
+
 router.post('/', (req, res) => {
-    res.send("login is working")
+    console.log(req.body);
 });
+
+
 export default router;
