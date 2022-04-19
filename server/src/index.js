9166db0c8598070661ed1e1d3e892701e18bfe66
AliReza <s.alireza.e99@gmail.com>
2022-04-19 23:20:46 +0430

    adding DB

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,8 @@ import Express from 'express';
 import login from './routes/login.js'
 import cors from 'cors';
 
-
-
 const app = Express();
-
-
-
 app.use(Express.json());
-
 
 const corsOptions ={
     origin:'http://localhost:3000', 
@@ -17,26 +11,8 @@ const corsOptions ={
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
-
-
-
-
-
-
 app.use('/login', login);
-
-
-
 app.listen(3030, () => console.log('listening on port 3030'));
 
-
-
-
-  
-
-// app.get("/api/login",(req, res) => {
-//     console.log("hello")
-
-// });
 
 // Use this after the variable declaration
--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -2,13 +2,9 @@ import { Router } from "express";
 
 const router = Router();
 
-
-
-
-
 router.post('/', (req, res) => {
     console.log(req.body);
+    res.send("hiiiiii");
 });
-
 
 export default router;
