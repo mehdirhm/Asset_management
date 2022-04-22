cd596bb83e3f065eabfa2fe546e91abd5a467d03
AliReza <s.alireza.e99@gmail.com>
2022-04-23 00:31:19 +0430

    hashing passwords to save in DB

--- server/src/routes/reguser.js
+++ server/src/routes/reguser.js
@@ -1,7 +1,7 @@
 // import auth from '../middleware/auth';
 // import jwt from 'jsonwebtoken';
 // import config from 'config';
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import _ from 'lodash';
 import { User, validate } from '../models/user.js';
 import { Router } from 'express';
@@ -12,23 +12,34 @@ const router = Router();
 //   res.send(user);
 // });
 
+//the post method of registering a new user
 router.post('/', async (req, res) => {
   const date = new Date();
+
+  //validating the user inputs
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
+  //avoid duplicated usernames
   let user = await User.findOne({ username: req.body.username });
   if (user) return res.status(400).send('User already registered.');
 
+  //creating a user object from the request data
   user = new User({
     fullname: req.body.fullname,
     username: req.body.username,
     password: req.body.password,
     regdate: date.getFullYear() +'-'+ ("0" + (date.getMonth() + 1)).slice(-2)+'-'+ ("0" + date.getDate()).slice(-2)
-  })
-//   const salt = await bcrypt.genSalt(10);
-//   user.password = await bcrypt.hash(user.password, salt);
+  });
+
+  //hashing the user password
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  //saving the new user in DB
   await user.save();
+
+
   res.send(user);
 //   const token = user.generateAuthToken();
 //   res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
