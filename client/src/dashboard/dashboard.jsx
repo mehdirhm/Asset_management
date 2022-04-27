421ced62ba51425c74e666f72a388f3d721cd4a6
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2022-04-27 23:51:10 +0430

    work on nav for dashboard panel

--- client/src/dashboard/dashboard.jsx
+++ client/src/dashboard/dashboard.jsx
@@ -15,6 +15,11 @@ import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { AppBar, Avatar, Badge, Box, IconButton, Toolbar, Tooltip } from '@mui/material';
 import theme from '../theme/index'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import { Users as UsersIcon } from '../icons/users';
+import { Bell as BellIcon } from '../icons/bell';
+
 
 
 const DashboardNavbarRoot = styled(AppBar)(({theme}) => ({
@@ -30,9 +35,10 @@ export const DashboardNavbar = (props) => {
   return (
     <>
         <DashboardNavbarRoot
+        dir="rtl"
         theme={theme}
         sx={{
-          left: {
+              right: {
             lg: 280
           },
           width: {
@@ -40,7 +46,56 @@ export const DashboardNavbar = (props) => {
           }
         }}
         {...other}>
-         sssss
+        <Toolbar
+          sx={{
+            minHeight : 64,
+            right : 0,
+            px: 2
+          }}
+          >
+            <IconButton
+              
+              onClick={onSidebarOpen}
+              sx={{
+
+                color:"primary",
+                
+                display : {
+                  xs: 'inline-block',
+                  lg: 'none'
+                }
+              }}
+            >
+               <MenuIcon fontSize="small" />
+
+            </IconButton>
+
+            <Tooltip title="Search">
+            <IconButton sx={{ mr: 1 , mt:1 }}>
+              <SearchIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+          <Box  sx={{ flexGrow: 1 }} />
+          <Tooltip title="Contacts">
+            <IconButton sx={{ mr: 1 }}>
+              <UsersIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="Notifications">
+            <IconButton sx={{ ml: 1 }}>
+              <Badge
+                badgeContent={4}
+                color="primary"
+                variant="dot"
+              >
+                <BellIcon fontSize="small" />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+
+          </Toolbar>
+
 
         </DashboardNavbarRoot>
         {/* <p>dashboard</p> */}
