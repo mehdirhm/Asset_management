e77ef59c2293fe083db199cf9b5060ae92c4992e
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2022-07-10 04:07:14 +0430

    send data for add assets to server

--- client/src/routes/add-assets.js
+++ client/src/routes/add-assets.js
@@ -3,16 +3,158 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Input from '@mui/material/Input';
-
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import FilledInput from '@mui/material/FilledInput';
+import MenuItem from '@mui/material/MenuItem';
+import axios from "axios";
+
+const types = [
+  {
+    value: 'sw',
+    label: 'Software',
+  },
+  {
+    value: 'hw',
+    label: 'Hardware',
+  },
+ 
+];
+
+const licenses = [
+  {
+    value: true,
+    label: 'Yes',
+  },
+  {
+    value: false,
+    label: 'No',
+  },
+ 
+];
 
 export default function AddAssets() {
 
 
     const [name, setName] = React.useState('');
-    const handleChange = (event) => {
+    const [serialNumber, setSerialNumber] = React.useState('');
+    const [description, setDescription] = React.useState('');
+    const [assetType, setAssetType] = React.useState('');
+    const [lastUpdate, setLastUpdate] = React.useState('');
+    const [installationDate, setInstallationDate] = React.useState('');
+    const [location, setLocation] = React.useState('');
+    const [position, setPosition] = React.useState('');
+    const [fullName, setFullName] = React.useState(''); 
+    const [isLicence, setIsLicence] = React.useState(false);
+    const [manufacturer, setManufacturer] = React.useState('');
+    const [type, setType] = React.useState('sw');
+    
+    
+
+
+
+    
+
+
+
+
+
+
+    const handleChangeName = (event) => {
       setName(event.target.value);
-      console.log(name);
+      // console.log(name);
+
+    };
+
+    const handleChangeSerialNumber = (event) => {
+      setSerialNumber(event.target.value);
+      // console.log(serialNumber);
+
+    };
+    const handleChangeLastUpdate = (event) => {
+      setLastUpdate(event.target.value);
+      // console.log(lastUpdate);
+
+    };
+    const handleChangeAssetType = (event) => {
+      setAssetType(event.target.value);
+      // console.log(name);
+
+    };
+    const handleChangeInstallationDate = (event) => {
+      setInstallationDate(event.target.value);
+      // console.log(name);
+
+    };
+    const handleChangeLocation = (event) => {
+      setLocation(event.target.value);
+      // console.log(name);
+
+    };
+    const handleChangeManufacturer = (event) => {
+      setManufacturer(event.target.value);
+      // console.log(name);
+
+    };
+    const handleChangeDescription = (event) => {
+      setDescription(event.target.value);
+      // console.log(name);
+
+    };
+    const handleChangeFullName = (event) => {
+      setFullName(event.target.value);
+      // console.log(name);
+
+    };
+
+    const handleChangePosition = (event) => {
+      setPosition(event.target.value);
+      // console.log(name);
+
+    };
+
+    const handleChangeIsLicence = (event) => {
+      setIsLicence(event.target.value);
+      // console.log(name);
+
+    };
+
+    const handleChangeType = (event) => {
+      setType(event.target.value);
+      // console.log(name);
+
+    };
+    const handleSubmit = (event) => {
+      // setIsLicence(event.target.value);
+      // console.log("name");
+
+      const newAsset = () => {
+
+
+
+
+        return {
+          type,
+          name,
+          serialNumber,
+          description,
+          assetType,
+          lastUpdate,
+          installationDate,
+          location,
+          position,
+          fullName,
+          isLicence,
+          manufacturer
+        }
+
+      }
+
+      axios.post('http://localhost:3030/assets', {
+      data: newAsset()
+      
+    });
+      console.log(newAsset());
 
     };
 
@@ -42,29 +184,43 @@ export default function AddAssets() {
           required
           id="outlined-required"
           label="Name"
-          onChange={handleChange}
+          onChange={handleChangeName}
           defaultValue="Hello World"
         />
         
         <TextField
           id="outlined-password-input"
           label="Serial Number"
-          type="number"
+          onChange={handleChangeSerialNumber}
           autoComplete="current-password"
         />
         <TextField
-          id="outlined-read-only-input"
-          label="Software"
-          defaultValue="sw"
-          InputProps={{
-            readOnly: true,
-          }}
+          id="outlined-required"
+          label="Asset Type"
+          onChange={handleChangeAssetType}
+         
+          
         />
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          value={type}
+          onChange={handleChangeType}
+          helperText="Please select your Type"
+        >
+          {types.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
 
 <TextField
           required
           id="outlined-required"
           label="Last Update"
+          onChange={handleChangeLastUpdate}
           type="date"
           focused        />
 
@@ -72,6 +228,7 @@ export default function AddAssets() {
             focused
           required
           label="Installation Date"
+          onChange={handleChangeInstallationDate}
           type="date"
           
         />
@@ -80,6 +237,7 @@ export default function AddAssets() {
 <TextField
           required
           id="outlined-required"
+          onChange={handleChangeLocation}
           label="Location"
           
         />  
@@ -87,6 +245,7 @@ export default function AddAssets() {
           required
           id="outlined-required"
           label="Manufacturer"
+          onChange={handleChangeManufacturer}
           defaultValue=""
         />  
 
@@ -94,6 +253,7 @@ export default function AddAssets() {
           required
           id="outlined-required"
           label="Description"
+          onChange={handleChangeDescription}
           defaultValue=""
         />  
 
@@ -101,6 +261,7 @@ export default function AddAssets() {
           required
           id="outlined-required"
           label="Full Name"
+          onChange={handleChangeFullName}
           defaultValue="Hello World"
         />
 
@@ -108,18 +269,39 @@ export default function AddAssets() {
           required
           id="outlined-required"
           label="Position"
+          onChange={handleChangePosition}
           defaultValue="Hello World"
         />
 
+
+
+
 <TextField
-          required
-          id="outlined-required"
+          id="outlined-select-currency"
+          select
           label="Is License"
-          defaultValue="Hello World"
-        />
+          value={isLicence}
+          onChange={handleChangeIsLicence}
+          helperText="Please select"
+        >
+          {licenses.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
         
         
       </div>
+      <Stack  >
+      <Button
+      onClick = {handleSubmit}
+        
+        
+       variant="contained" color="success">
+        Save
+      </Button>
+      </Stack>
     </Box>
     </>
   );
